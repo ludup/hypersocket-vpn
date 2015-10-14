d5c275b57cb73cc3c4001ad0321163be3ceb7ef4
Lee David Painter <lee@ludup.com>
2015-10-14 14:57:37 +0100

    New resource chart.

--- hypersocket-network/src/main/java/com/hypersocket/network/events/NetworkResourceSessionEvent.java
+++ hypersocket-network/src/main/java/com/hypersocket/network/events/NetworkResourceSessionEvent.java
@@ -41,4 +41,8 @@ public class NetworkResourceSessionEvent extends ResourceSessionEvent {
 	public String[] getResourceKeys() {
 		return ArrayUtils.add(super.getResourceKeys(), EVENT_RESOURCE_KEY);
 	}
+	
+	public boolean isUsage() {
+		return false;
+	}
 }
