ac049ebead1092a7b2f3e6b0a51428d86b236858
Lee David Painter <lee@ludup.com>
2016-08-09 16:27:09 +0100

    Bumped version to 1.2.3-LOCAL

--- hypersocket-network/src/main/java/com/hypersocket/launcher/ApplicationLauncherResourceServiceImpl.java
+++ hypersocket-network/src/main/java/com/hypersocket/launcher/ApplicationLauncherResourceServiceImpl.java
@@ -112,8 +112,6 @@ public class ApplicationLauncherResourceServiceImpl extends
 				ApplicationLauncherResourcePermission.DELETE),
 				NetworkResourceServiceImpl.MENU_NETWORK);
 
-		menuService.registerExtendableTable(APPLICATION_LAUNCHER_ACTIONS);
-		
 		eventService.registerEvent(ApplicationLauncherEvent.class,
 				RESOURCE_BUNDLE, this);
 		eventService.registerEvent(ApplicationLauncherCreatedEvent.class,
